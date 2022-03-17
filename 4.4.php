9b11abeedc9fc54b8ff24b405189cd61e45edbb7
laczkowska <91725304+laczkowska@users.noreply.github.com>
2022-03-17 18:10:21 +0100

    Add files via upload

--- 4.4.php
+++ 4.4.php
@@ -1,22 +1,26 @@
 <?php
-$n = 5;
-if ($n < 1) {
-    print ("To pojęcie nie ma zastosowania dla $n.");
-} elseif ($n == 1) {
-    print ('Liczba 1 nie jest pierwsza');
-} else {
-    $pierwsza = True;
-    for ($i = 2; $i < $n; $i++) {
+
+$n = 16127;
+
+function p($n){
+    $a = 0;
+    if ($n == 1 || $n == 0) {
+        echo ("Liczba $n nie jest pierwsza, $a");
+    } else {
+        $pierwsza = True;
+        for ($i = 2; $i < $n; $i++) {
+            if ($pierwsza) {
+                if ($n % $i == 0) {
+                    $pierwsza = False;
+                }
+            }$a++;
+        }
+
         if ($pierwsza) {
-            if ($n % $i == 0) {
-                $pierwsza = False;
-            }
+            echo ("Liczba $n jest pierwsza, $a");
+        } else {
+            echo ("Liczba $n nie jest pierwsza, $a");
         }
     }
 }
-if ($pierwsza) {
-    print ("Liczba $n jest pierwsza.");
-} else {
-    print ("Liczba $n nie jest pierwsza.");
-}
-?>
+echo p($n);
