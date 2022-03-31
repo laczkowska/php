d017dab1ff7ea0e1222696c83a4f7ff390b12e32
laczkowska <91725304+laczkowska@users.noreply.github.com>
2022-03-31 18:05:48 +0200

    Delete zadanka 1 i 2 directory

--- zadanka 1 i 2/2.2.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$r = 3;
-
-function d($r){
-    $d = $r * 2;
-    echo $d;
-}
-d($r);
--- zadanka 1 i 2/zad1.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$a = 3;
-
-if ($a %2==0){
-    echo 'parzysta';
-} else{
-    echo 'nieparzysta';
-}
