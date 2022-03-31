38b3490299328910cee0d1f16a0782cc07830867
laczkowska <91725304+laczkowska@users.noreply.github.com>
2022-03-31 17:37:19 +0200

    Add files via upload

---
+++ zadanka 1 i 2/z1.2.php
@@ -0,0 +1,20 @@
+<?php
+
+$a = 6;
+
+switch($a){
+    case 1:
+        echo "poniedziałek"; break;
+    case 2:
+        echo "wtorek"; break;
+    case 3:
+        echo "środa"; break;
+    case 4:
+        echo "czwartek"; break;
+    case 5:
+        echo "piątek"; break;
+    case 6:
+        echo "sobota"; break;
+    case 7:
+        echo "niedziela"; break;
+}
