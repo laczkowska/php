b6255aaef899def9f3226b36730ae8aab36822c2
laczkowska <91725304+laczkowska@users.noreply.github.com>
2022-05-12 16:59:08 +0200

    Delete pokemon directory

--- pokemon/index.php
+++
@@ -1,8 +0,0 @@
-<?php
-include 'ice.php';
-include 'dragon.php';
-include 'walka.php';
-$p1 = new ice('Cloyster','ice',100);
-$p2 = new dragon('Exeguttor','dragon',180);
-$walka = new walka($p1, $p2);
-$walka -> go();
